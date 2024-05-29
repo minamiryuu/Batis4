27a3eb0ecdf58aa4e6cf1a3c36b2fbfcbc3c3e9c
minamiryuu <minami.ryusei@itfllc.co.jp>
2024-05-29 10:41:43 +0900

    Spring Boot＋Postgres＋MyBatis

---
+++ src/main/java/com/numacci/api/DemoB4Application.java
@@ -0,0 +1,13 @@
+package com.numacci.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoB4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoB4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/numacci/api/DemoB4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numacci.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoB4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
